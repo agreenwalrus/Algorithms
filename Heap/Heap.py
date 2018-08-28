5a1e56bf9857f23ee34ac99f67e19911a4411a5c
Julia Runova <jul.runova@gmail.com>
2018-08-28 09:11:26 +0300

    Heap iterface

---
+++ Heap/Heap.py
@@ -0,0 +1,13 @@
+class Heap:
+
+    def __init__(self, size=100):
+        self.max_amount_of_elements = size
+        self.memory = [(None, None) * self.size]
+        self.size = 0
+
+    def insert(self, priority, element):
+        pass
+
+    def extract_min(self):
+        pass
+
