c123791486e2749ed33a3daa12b3c6ce1398f9be
Julia Runova <jul.runova@gmail.com>
2018-08-28 11:20:39 +0300

    method insert is realized

--- Heap/Heap.py
+++ Heap/Heap.py
@@ -2,11 +2,38 @@ class Heap:
 
     def __init__(self, size=100):
         self.max_amount_of_elements = size
-        self.memory = [(None, None) * self.size]
+        self.memory = [(None, None) for _ in range(self.max_amount_of_elements + 1)]
         self.size = 0
 
+    def __str__(self):
+        res = ""
+        for elem in self.memory[1:]:
+            res = res + "priority: " + str(elem[0]) + ' - ' + str(elem[1]) + "\n"
+        return res
+
     def insert(self, priority, element):
-        pass
+
+        def update():
+            child_index = self.size
+            parent_index = child_index >> 1
+            while parent_index > 0 and self.memory[parent_index] > self.memory[child_index]:
+                l_child_index = parent_index << 1
+                r_child_index = l_child_index + 1
+                min_child_index = \
+                    l_child_index \
+                    if not l_child_index ^ self.size or self.memory[l_child_index] < self.memory[r_child_index]\
+                    else r_child_index
+                self.memory[min_child_index], self.memory[parent_index] = \
+                    self.memory[parent_index], self.memory[min_child_index]
+                child_index = parent_index
+                parent_index = child_index >> 1
+
+        if self.size < self.max_amount_of_elements:
+            self.size += 1
+            self.memory[self.size] = (priority, element)
+            update()
+        else:
+            raise MemoryError("no more memory")
 
     def extract_min(self):
         pass
--- Heap/main.py
+++ Heap/main.py
@@ -1,7 +1,24 @@
+import Heap as heap
 
 
 def main():
-    pass
+    h = heap.Heap(5)
+    print(h)
+
+    h.insert(2, 2)
+    print(h)
+
+    h.insert(1, 1)
+    print(h)
+
+    h.insert(1, 1)
+    print(h)
+
+    h.insert(3, 3)
+    print(h)
+
+    h.insert(0, 0)
+    print(h)
 
 
 if __name__ == "__main__":
