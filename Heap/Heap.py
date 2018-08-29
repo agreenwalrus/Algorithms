447672e3750e881aa4d5b7c9c8086bd1c995be8b
Julia Runova <jul.runova@gmail.com>
2018-08-29 08:47:31 +0300

    extract_min is realized

--- Heap/Heap.py
+++ Heap/Heap.py
@@ -1,40 +1,63 @@
-class Heap:
+class MinHeap:
 
     def __init__(self, size=100):
         self.max_amount_of_elements = size
-        self.memory = [(None, None) for _ in range(self.max_amount_of_elements + 1)]
+        self.memory = [(None, None) for _ in range(self.max_amount_of_elements)]
         self.size = 0
 
     def __str__(self):
         res = ""
-        for elem in self.memory[1:]:
+        for elem in self.memory:
             res = res + "priority: " + str(elem[0]) + ' - ' + str(elem[1]) + "\n"
         return res
 
     def insert(self, priority, element):
 
         def update():
-            child_index = self.size
-            parent_index = child_index >> 1
-            while parent_index > 0 and self.memory[parent_index] > self.memory[child_index]:
-                l_child_index = parent_index << 1
-                r_child_index = l_child_index + 1
-                min_child_index = \
-                    l_child_index \
-                    if not l_child_index ^ self.size or self.memory[l_child_index] < self.memory[r_child_index]\
-                    else r_child_index
-                self.memory[min_child_index], self.memory[parent_index] = \
-                    self.memory[parent_index], self.memory[min_child_index]
-                child_index = parent_index
-                parent_index = child_index >> 1
+            child_index = self.size - 1
+            parent_index = (child_index - 1) >> 1
+            if self.size > 1:
+                while self.memory[parent_index] > self.memory[child_index]:
+                    l_chld_ind = (parent_index << 1) + 1
+                    r_chld_ind = l_chld_ind + 1
+                    min_child_index = \
+                        l_chld_ind \
+                        if not l_chld_ind ^ (self.size - 1) or self.memory[l_chld_ind] < self.memory[r_chld_ind]\
+                        else r_chld_ind
+                    self.memory[min_child_index], self.memory[parent_index] = \
+                        self.memory[parent_index], self.memory[min_child_index]
+                    child_index = parent_index
+                    parent_index = child_index >> 1
 
         if self.size < self.max_amount_of_elements:
-            self.size += 1
             self.memory[self.size] = (priority, element)
+            self.size += 1
             update()
         else:
             raise MemoryError("no more memory")
 
     def extract_min(self):
-        pass
+
+        min_priority = self.memory[0]
+        last_el_ind = self.size - 1
+        self.size -= 1
+
+        parent_index = 0
+        while parent_index != last_el_ind:
+            l_chld_ind = (parent_index << 1) + 1
+            r_chld_ind = l_chld_ind + 1
+            if r_chld_ind <= self.size:
+                if self.memory[l_chld_ind] <= self.memory[r_chld_ind] and self.memory[l_chld_ind] < self.memory[last_el_ind]:
+                    min_child_index = l_chld_ind
+                elif self.memory[r_chld_ind] < self.memory[l_chld_ind] and self.memory[r_chld_ind] < self.memory[last_el_ind]:
+                    min_child_index = r_chld_ind
+            else:
+                min_child_index = last_el_ind
+
+            self.memory[parent_index] = self.memory[min_child_index]
+            parent_index = min_child_index
+
+        self.memory[self.size] = (None, None)
+        return min_priority
+
 
--- Heap/main.py
+++ Heap/main.py
@@ -2,22 +2,45 @@ import Heap as heap
 
 
 def main():
-    h = heap.Heap(5)
+    h = heap.MinHeap(5)
+    print(h)
+
+
+
+    #h.insert(1, 1)
+    #print(h)
+
+    h.insert(1, 1)
     print(h)
 
     h.insert(2, 2)
     print(h)
 
-    h.insert(1, 1)
-    print(h)
-
-    h.insert(1, 1)
-    print(h)
-
     h.insert(3, 3)
     print(h)
 
-    h.insert(0, 0)
+    h.insert(1, 1)
+    print(h)
+
+    #h.insert(0, 0)
+    #print(h)
+
+    h.extract_min()
+    print(h)
+
+    h.extract_min()
+    print(h)
+
+    h.extract_min()
+    print(h)
+
+    h.extract_min()
+    print(h)
+
+    h.extract_min()
+    print(h)
+
+    h.extract_min()
     print(h)
 
 
